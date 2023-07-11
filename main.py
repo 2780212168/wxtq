a902e8f3bf04dc98d3fc62b6f8fe945b4fe031c9
2780212168 <82633987+2780212168@users.noreply.github.com>
2023-07-11 22:02:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ import os
  
 def get_color():
     # 获取随机颜色
-    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
-    color_list = get_colors(100)
-    return random.choice(color_list)
+    color = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])]
+    return color[0]
+    #get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
+    #color_list = get_colors(100)
+    #return random.choice(color_list)
  
  
 def get_access_token():
